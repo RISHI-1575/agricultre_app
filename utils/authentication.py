3858666ad3ec5e87d31306bf38eb825487efef28
RISHI-1575 <23aiml050@bnmit.in>
2025-04-27 23:07:15 +0530

    Update authentication.py

--- utils/authentication.py
+++ utils/authentication.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def authenticate_user(username, password, role):
+    try:
+        df = pd.read_csv("data/credentials.csv")
+        user = df[(df['username'] == username) & (df['password'] == password) & (df['role'] == role)]
+        return not user.empty
+    except FileNotFoundError:
+        return False
