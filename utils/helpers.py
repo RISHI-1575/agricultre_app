5e0f7d649261f628c5f260e9c0281544624a8349
RISHI-1575 <23aiml050@bnmit.in>
2025-04-27 23:09:02 +0530

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,6 @@
-
+def get_tips():
+    return [
+        "Water your crops early in the morning.",
+        "Rotate crops to maintain soil quality.",
+        "Use organic fertilizers for better yields."
+    ]
