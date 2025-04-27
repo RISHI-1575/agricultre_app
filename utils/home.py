77648101fa4ea2f20e3a784737bdc263e14a387f
RISHI-1575 <23aiml050@bnmit.in>
2025-04-27 23:07:32 +0530

    Update home.py

--- utils/home.py
+++ utils/home.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from utils.weather_api import get_weather
+from utils.helpers import get_tips
 
+def home_page():
+    st.title("Home - Agriculture App")
+
+    # Area Selection
+    area = st.selectbox("Select Your Area", ["Karnataka"])
+
+    # Fetch Weather
+    weather = get_weather(area)
+    st.write("### Weather Today:", weather)
+
+    # Farming Tips
+    tips = get_tips()
+    st.write("### Farming Tips:", tips)
